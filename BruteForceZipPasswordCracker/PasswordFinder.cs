83a4af2230fd95b2d89a7f3d6edc94a6f2621737
Imortalco <vanko.krastev@gmail.com>
2022-05-01 19:33:31 +0300

    Add project files.

---
+++ BruteForceZipPasswordCracker/PasswordFinder.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using Ionic.Zip;
+
+namespace BruteForceZipPasswordCracker
+{
+    internal class PasswordFinder
+    {
+        //Exception in case Zip file does not exist
+        class ZipFileNotExistentException : Exception
+        { }
+
+        // Exception in case Zip file is not password protected
+        class ZipFileNotPasswordProtectedException : Exception
+        { }
+        public string FoundPassword;
+
+        private string fileName;
+        private TaskCompletionSource<string> passwordResponse;
+        private BlockingCollection<string> passwordQueue;
+        
+        public PasswordFinder(string fileName,TaskCompletionSource<string> passwordResponse)
+        {
+            this.fileName = fileName;
+            this.passwordResponse = passwordResponse;
+        }
+
+        public async Task Run()
+        {
+            await Task.Run(() => this.FindPassword());
+        }
+
+        private void FindPassword()
+        {
+            if (!File.Exists(fileName))
+            {
+                passwordResponse.TrySetException(new ZipFileNotExistentException);
+            }
+            else if(!IsPasswordProtected())
+            {
+                passwordResponse.TrySetException(new ZipFileNotPasswordProtectedException);
+            }
+
+            while (!passwordResponse.Task.IsCompleted)
+            {
+                string currentPassword = passwordQueue.Take();
+
+                bool passwordFound = TryPassword(currentPassword);
+
+                if (passwordFound)
+                {
+                    passwordResponse.SetResult(currentPassword);
+                }
+            }
+        }
+
+        private bool IsPasswordProtected()
+        {
+            return !ZipFile.CheckZipPassword(fileName, "");
+        }
+
+        public bool TryPassword(string password)
+        {
+            bool passwordFound = false;
+
+            try
+            {
+                passwordFound = ZipFile.CheckZipPassword(fileName,password);
+                if (passwordFound == true)
+                {
+                    this.FoundPassword = password;
+                }
+            }
+            catch { }
+
+            return passwordFound;
+        }
+    }
+}
---
+++ BruteForceZipPasswordCracker/PasswordGenerator.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace BruteForceZipPasswordCracker
+{
+    internal class PasswordGenerator
+    {
+        private StringBuilder passwordBuilder;
+        private BlockingCollection<string> passwordQueue;
+        private CancellationToken cancellationToken;
+
+        public PasswordGenerator(string initialPassword = "", BlockingCollection<string> passwordQueue)
+        {
+            this.passwordBuilder = new StringBuilder(initialPassword);
+            this.passwordQueue = passwordQueue;
+        }
+
+        public async Task Run()
+        {
+            await Task.Run(() => this.AddNextPasswordToQueue());
+        }
+
+        private void AddNextPasswordToQueue()
+        {
+            try
+            {
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    string nextPassword = NextPassword();
+                    passwordQueue.Add(nextPassword, cancellationToken);
+                }
+            }
+            catch(OperationCanceledException)
+            { }
+        }
+        private string NextPassword()
+        {
+            string currentPassword = passwordBuilder.ToString();
+
+            GenerateNextPassword();
+
+            return currentPassword;
+        }
+
+        private void GenerateNextPassword()
+        {
+            if(passwordBuilder.Length == 0)
+            {
+                AddNewDigit();
+            }
+            else
+            {
+                int counter = 0;
+                bool stop = false;
+
+                while (!stop)
+                {
+                    passwordBuilder[counter] = (char)NextDigit(int.Parse(passwordBuilder[counter].ToString()));
+
+                    if(passwordBuilder[counter] == '0')
+                    {
+                        counter++;
+
+                        if(counter == passwordBuilder.Length)
+                        {
+                            AddNewDigit();
+                            stop = true;
+                        }
+                    }
+                    else
+                    {
+                        stop = true;
+                    }
+                }
+            }
+        }
+
+        private void AddNewDigit()
+        {
+            passwordBuilder.Append('0');
+        }
+
+        private int NextDigit(int currentDigit)
+        {
+            int nextDigit;
+            
+            if(currentDigit == 9)
+            {
+                nextDigit = 0;
+            }
+            else
+            {
+                nextDigit = ++currentDigit;
+            }
+
+            return nextDigit;
+        }
+
+    }
+}
